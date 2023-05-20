6f73d1c89ff965d1e1c9694050c33c9bccf55319
jIVOYz <ilaprohorov51@gmail.com>
2023-05-20 15:19:42 +0300

    commit message

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swipe-button-next',
+    prevEl: '.swipe-button-prev',
+  },
+  pagination: {
+    pagination: true,
+  },
+})
 
-    navigation:{
-        nextEl: '.swipe-button-next',
-        prevEl:'.swipe-button-prev',
-    },
-    pagination: {
-        pagination: true,
-    },
-});
+//* Burger menu
+const menu = document.querySelector('.menu')
+const menuBtn = document.querySelector('.hamburger-lines')
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('active')
+})
